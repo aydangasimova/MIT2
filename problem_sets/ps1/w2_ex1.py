b4e8a052c533c56212c87fe4df97aee50bcfdf60
aydangasimova <a.gsmva@gmail.com>
2019-10-29 16:49:00 +0100

    added the actual solution to the problem

--- problem_sets/ps1/w2_ex1.py
+++ problem_sets/ps1/w2_ex1.py
@@ -39,15 +39,16 @@ def powerSet(items):
         combo = []
         for j in range(N):
             # test bit jth of integer i
-            if (i >> j) % 2 == 1:
+            if (i // 2**j) % 2 == 1:
                 combo.append(items[j])
+                print('appending', items[j])
         yield combo
 
 my_gen = powerSet([1,2,3])
 
 for i in my_gen:
-    print(i)
-
+     print(i)
+'''
 def yieldAllCombos(items):
     """
         Generates all combinations of N items into two bags, whereby each
@@ -55,18 +56,56 @@ def yieldAllCombos(items):
 
         Yields a tuple, (bag1, bag2), where each bag is represented as a list
         of which item(s) are in each bag.
-    """
-    # generate all combinations of N items
+    # """
+    # generate all combinations of N items into two bags
     N = len(items)
-    # enumerate the 3**N possible combinations
     for i in range(3**N):
         bag1 = []
         bag2 = []
         for j in range(N):
             # test bit jth of integer i
-            if (i // 3 ** j) % 3 == 0:
-                bag_1.append(items[j])
-            elif (i // 3 ** j) % 3 == 1:
-                bag_2.append(items[j])
-        yield (bag1, bag2)
+            # if
+                bag1.append(items[j])
+            # if
+                bag2.append(items[j])
+            combo = (bag1, bag2)
+    yield combo
 
+
+# my_gen2 = yieldAllCombos(['a','b'])
+#
+# count = 0
+# for i in my_gen2:
+#     print(i)
+#     count += 1
+'''
+print("---------------------")
+
+
+def yieldAllCombos(items):
+    N = len(items)
+    # enumerate the 3**N possible combinations
+    for i in range(3**N):
+        bag_1 = []
+        bag_2 = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i // 3**j) % 3 == 0:
+                bag_1.append(items[j])
+            elif (i // 3**j) % 3 == 1:
+                bag_2.append(items[j])
+        yield (bag_1, bag_2)
+
+
+my_gen = yieldAllCombos([1, 2])
+
+count = 0
+for i in my_gen:
+    print(i)
+    count += 1
+
+print(count)
+
+
+def f2(items):
+    N = len(items)
