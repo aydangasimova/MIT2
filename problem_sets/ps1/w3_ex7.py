c24daada33014922f7da7c34cc3e503832288452
aydangasimova <a.gsmva@gmail.com>
2019-11-11 17:20:17 +0100

    started w3_ex7

---
+++ problem_sets/ps1/w3_ex7.py
@@ -0,0 +1,12 @@
+from ps1.lecture_handout2 import *
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight):
+        # Your code here
+        pass
+    def getWeight(self):
+        # Your code here
+        pass
+    def __str__(self):
+        # Your code here
+        pass
